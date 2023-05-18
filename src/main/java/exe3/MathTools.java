dd99941add0c3194b9778da29eda7a245232e828
Officialravi888 <youremail@yourdomain.com>
2023-05-18 11:35:25 +0530

    mathexample

--- src/main/java/exe3/MathTools.java
+++ src/main/java/exe3/MathTools.java
@@ -1,4 +1,10 @@
 package exe3;
 
 public class MathTools {
+    public static double convertToDecimal(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("b must not be 0");
+        }
+        return (double)a / (double)b;
+    }
 }
--- src/test/java/exe3/MathToolsTest.java
+++ src/test/java/exe3/MathToolsTest.java
@@ -1,7 +1,20 @@
 package exe3;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 class MathToolsTest {
-
+    @Test
+    void should_test_return_true_when_ConvertToDecimalSuccess() {
+        //given
+        int a=3;
+        int b=4;
+        //when
+        double result = MathTools.convertToDecimal(3, 4);
+        //then
+        Assertions.assertEquals(0.75, result);
+    }
+    @Test
+    void testConvertToDecimalInvalidDenominator() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> MathTools.convertToDecimal(3, 0));
+    }
 }
